19ecac4bd08ea436b4eb2f1a811ad2f9912948a6
Pat Laughlin <plaugh1@umbc.edu>
2010-12-13 18:48:32 -0500

    Macros have been fixed, with coalesce fleshing out more.

--- mm.c
+++ mm.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -13,25 +14,28 @@
  ********************************************************/
 team_t team = {
     /* Team name */
-    "dorman1+plaug1",
+    "xx+xx",
     /* First member's full name */
-    "Alexander Dorfman",
+    "xx",
     /* First member's email address */
-    "dorfman1@umbc.edu",
+    "xx",
     /* Second member's full name (leave blank if none) */
-    "Pat Laughlin",
+    "xx",
     /* Second member's email address (leave blank if none) */
-    "plaugh1@umbc.edu"
+    "xx"
 };
 
 struct s_block {
-    size_t size; //Contains size & allocation bit
-    struct s_block *ptr; //Pointer to Next/Prev
+	size_t size; //Contains size & allocation bit
+	struct s_block *ptr; //Pointer to Next/Prev
 };
 
 typedef struct s_block *t_block;
 
 static struct s_block *heap_listp;
+
+
+
 
 /* single word (4) or double word (8) alignment */
 #define WSIZE 4
@@ -41,26 +45,39 @@ static struct s_block *heap_listp;
 #define FREE 0
 #define USED 1
 
-/* macros used for getting information about the block */
-#define HDRP(bp) (bp - 1)
-#define FTRP(bp) (bp + (GET_SIZE(HDRP(bp)-> size) - OVERHEAD)/DSIZE) //needs corrections !!!!!!!
-#define EPILOG(bp) (FTRP(bp)+1)
-#define PREV_FTRP(bp) (bp - 2)
-#define NEXT_HDRP(bp)(FTRP(bp) + 1)
+
+#define HDRP(bp) (bp - 1) //Correct
+#define FTRP(bp) (bp + ((GET_SIZE(HDRP(bp)->size) - OVERHEAD) / DSIZE)) //Correct
+#define EPILOG(bp) (FTRP(bp)+1) //Correct
+
+#define PREV_BLOCK(bp) (bp - (GET_SIZE(PREV_FTRP(bp)->size) / DSIZE)) //Correct
+#define NEXT_BLOCK(bp) (bp + (GET_SIZE(HDRP(bp)->size) / DSIZE)) //bp + SIZE/DSIZE
+
+
+#define PREV_HDRP(bp) (HDRP(PREV_BLOCK(bp))) //Correct
+#define PREV_FTRP(bp) (bp - 2)  //Correct
+
+#define NEXT_HDRP(bp) (HDRP(NEXT_BLOCK(bp))) //Correct
+#define NEXT_FTRP(bp) (FTRP(NEXT_BLOCK(bp))) //Correct
+
+
+
+
+
 
 // Read the size and allocated fields from address p
+
 #define GET_SIZE(s)  (s & ~0x7)
 #define GET_ALLOC(s) (s & 0x1)
 
-/* Used for reading and writing data */
-inline size_t GET(void *p) { return  * (size_t*)p; }
-inline void PUT( void *p, size_t val) { *(size_t *)p = val; }
 
 /* rounds up to the nearest multiple of ALIGNMENT */
 #define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
 /* Pack a size and allocated bit into a word */
 #define PACK(size, alloc)  ((size) | (alloc))
 /* Given block ptr bp, compute address of its header and footer */
+
+
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
@@ -70,38 +87,38 @@ static void printblock(t_block bp);
 static void printtag(t_block bp);
 static void *coalesce(t_block bp);
 
+
 int mm_init(void)
 {
 
-    t_block prolog_header, prolog_footer, epilog;
+	t_block prolog_header, prolog_footer, epilog;
 
-    //printf("sizeof struct:%d\n",sizeof(struct s_block));
+	//printf("sizeof struct:%d\n",sizeof(struct s_block));
 
-    //try to allocated 3* Struct(8bytes) = 24 bytes
-
-    if ((heap_listp = mem_sbrk(3*sizeof(struct s_block))) == NULL)
-        return -1;
-
-
-     prolog_header = heap_listp;
-     prolog_header->size = PACK(0,USED);
-
-     prolog_footer = heap_listp + 1;
-     epilog = heap_listp + 2;
-
-     prolog_header->size = PACK(0,USED);
-
-     prolog_footer->size = PACK(0,USED);
+	//try to allocated 3* Struct(8bytes) = 24 bytes
+	if ((heap_listp = mem_sbrk(3*sizeof(struct s_block))) == NULL)
+		return -1;
 
 
-     epilog->size = PACK(0,USED);
-     printtag(epilog);
+	 prolog_header = heap_listp;
+	 prolog_header->size = PACK(0,USED);
 
-     heap_listp += 1;
+	 prolog_footer = heap_listp + 1;
+	 epilog = heap_listp + 2;
 
-     //printblock(heap_listp);
+	 prolog_header->size = PACK(0,USED);
 
-     return 0;
+	 prolog_footer->size = PACK(0,USED);
+
+
+
+	 epilog->size = PACK(0,USED);
+	 printtag(epilog);
+
+	 heap_listp += 1;
+
+	 //printblock(heap_listp);
+	 return 0;
 
 }
 
@@ -111,53 +128,43 @@ int mm_init(void)
  */
 void *mm_malloc(size_t size)
 {
-    size = 11;
-    size_t asize;      /* adjusted block size */
-    char *bp;
+	size = 11;
+	size_t asize;      /* adjusted block size */
+	char *bp;
 
     // Ignore spurious requests
     if (size <= 0)
-        return NULL;
+    	return NULL;
     // Adjust block size to include overhead and alignment reqs.
     if (size <= DSIZE)
-        asize = DSIZE + OVERHEAD;
+    	asize = DSIZE + OVERHEAD;
     else
-        asize = DSIZE * ((size + (OVERHEAD) + (DSIZE-1)) / DSIZE);
+    	asize = DSIZE * ((size + (OVERHEAD) + (DSIZE-1)) / DSIZE);
 
     if ((bp = extend_heap(asize)) == NULL)
-        return NULL;
+    	return NULL;
 
 
     /*
     // Search the free list for a fit
     if ((bp = find_fit(asize)) != NULL) {
-        place(bp, asize);
-        return bp;
+    	place(bp, asize);
+    	return bp;
     }
 
     // No fit found. Get more memory and place the block
     extendsize = MAX(asize,CHUNKSIZE);
     if ((bp = extend_heap(extendsize/WSIZE)) == NULL)
-        return NULL;
+    	return NULL;
     place(bp, asize);
     return bp;
     */
 
-    return 0;
-
+	return 0;
 
 }
-/**
- * mm_free - this simply clears the header and footer of the passed
- *      block, and then coalesces.
- */)
 void mm_free(void *ptr)
 {
-    size_t size = GET_SIZE(ptr->size)
-
-    PUT(HDRP(ptr), PACK(size, FREE));
-    PUT(FTRP(ptr), PACK(size, FREE));
-    coalesce(ptr);
 }
 
 void *mm_realloc(void *ptr, size_t size)
@@ -178,7 +185,6 @@ void *mm_realloc(void *ptr, size_t size)
 }
 
 static void printblock(t_block bp)
-
 {
     size_t head_size, head_alloc, foot_size, foot_alloc;
     t_block header_ptr, footer_ptr;
@@ -186,19 +192,21 @@ static void printblock(t_block bp)
     header_ptr = HDRP(bp);
     footer_ptr = FTRP(bp);
 
+
+
     head_size = GET_SIZE(header_ptr->size);
     head_alloc = GET_ALLOC(header_ptr->size);
     foot_size = GET_SIZE(footer_ptr->size);
     foot_alloc = GET_ALLOC(footer_ptr->size);
 
     if (head_size == 0) {
-        printf("%p: EOL\n", bp);
-        return;
+    	printf("%p: EOL\n", bp);
+    	return;
     }
 
     printf("%p: header: [%d:%c] footer: [%d:%c]\n", bp,
-            head_size, (head_alloc ? 'a' : 'f'),
-       foot_size, (foot_alloc ? 'a' : 'f'));
+    		head_size, (head_alloc ? 'a' : 'f'),
+	   foot_size, (foot_alloc ? 'a' : 'f'));
 }
 
 static void printtag(t_block bp)
@@ -209,12 +217,12 @@ static void printtag(t_block bp)
     head_alloc = GET_ALLOC(bp->size);
 
     if (head_size == 0) {
-        printf("\n%p: EOL\n", bp);
-        return;
+    	printf("\n%p: EOL\n", bp);
+    	return;
     }
 
     printf("\n%p: header: [%d:%c]", bp,
-            head_size, (head_alloc ? 'a' : 'f'));
+    		head_size, (head_alloc ? 'a' : 'f'));
 }
 
 static void *extend_heap(size_t words) //words already includes
@@ -224,7 +232,7 @@ static void *extend_heap(size_t words) //words already includes
 
     // Allocate an even number of words to maintain alignment
     if ((int)(start = mem_sbrk(words)) == -1)
-        return NULL;
+    	return NULL;
     //overwrite previous epilog with new header
     header = HDRP(start);
     header->size = PACK(words,FREE);
@@ -236,12 +244,12 @@ static void *extend_heap(size_t words) //words already includes
     printtag(footer);
     //calculate new epilog
     epilog = EPILOG(start);
-
     epilog->size = PACK(0,USED);
     printtag(epilog);
 
     /* Coalesce if the previous block was free */
-    coalesce(start);
+    //start+1 == to start from payload, rather than footer
+    t_block test = coalesce(start+1);
     return NULL;
 }
 
@@ -250,73 +258,93 @@ static void *coalesce(t_block bp)
 	//bp points to the Header
 	//bp - 1 == footer of previous block !!!
 
+	//get allocation of the Prev block
 	size_t prev_alloc = GET_ALLOC(PREV_FTRP(bp)->size);
+	//get allocation of the Next block
 	size_t next_alloc =   GET_ALLOC(NEXT_HDRP(bp)->size);
-	size_t size = bp->size;
+	//get size of the current block
+	size_t curr_size = GET_SIZE(bp->size);
 
-	// nothing to coalesce
+	//Case 1: Nothing to coalesce
 	if (prev_alloc && next_alloc) {
 		return bp;
 	}
 
-    // coalesce with the next block
+    // Case 2: Coalesce with next block
+	// remember USED == 1
     else if (prev_alloc) {
-        t_block next = NEXT_HDRP(bp);
+    	//pointer to the next Footer
+        t_block next_FT = NEXT_FTRP(bp);
 
-        // clear them from the free lists, so we can
-        // coalesce them together
-        delete_from_free_list(bp);
-        delete_from_free_list(next);
+        // Will need to be removed from free list
+        // Maintain free lists
+        // delete_from_free_list(block);
+        // delete_from_free_list(next);
 
-        // Update header of BLOCK so that size stretches to encompass NEXT
-        size += next->size;
-        PUT(HEADER(bp), PACK(size,FREE,IS_PREV_ALLOC(bp)));
-        PUT(FOOTER(next), PACK(size,FREE,IS_PREV_ALLOC(bp)));
+        // Update header of current block so that size stretches to encompass NEXT
+        curr_size += GET_SIZE(next_FT->size);
+        bp->size = PACK(curr_size,FREE);
+        // Update footer of current block so that size stretches to encompass PREV
+        next_FT->size = PACK(curr_size,FREE);
 
-        push_free_list(bp);
-        return bp;
-    // coalesce with the previous block
-    } else if (next_alloc) {
-        t_block prev = PREV_HDRP(bp);
-        
-        // clear them from the free lists, so we can
-        // coalesce them together
-        delete_from_free_list(bp);
-        delete_from_free_list(prev);
-        
-        size += prev->size;
-        PUT(HEADER(prev), PACK(size, FREE));
-        PUT(FOOTER(bp), PACK(size, FREE));
-        
-        // add the entire block to the free list
-        push_free_list(prev);
-        return prev;
-    // coalesce with both the prev and next
-    } else {
-        t_block prev = PREV_HDRP(bp),
-                next = NEXT_HDRP(bp);
-                
-        delete_from_free_list(bp);
-        delete_from_free_list(next);
-        delete_from_free_list(prev);
-        
-        size += next->size + prev->size;
-        PUT(HEADER(prev), PACK(size, FREE));
-        PUT(FOOTER(next), PACK(size, FREE));
-        
-        // add the entire block to the free list
-        push_free_list(prev);
-        return prev;
+        //spit();
+
+        //return;
     }
 
+    // Case 3: Coalesce with prev block
+    else if (next_alloc) {
+        t_block prev_HD = PREV_HDRP(bp);
+        size_t prev_size = GET_SIZE(prev_HD->size);
+
+        // Maintain free lists
+        //delete_from_free_list(block);
+        //delete_from_free_list(prev);
+
+        // Update header of PREV so that size stretches to encompass BLOCK
+        prev_size += curr_size;
+        prev_HD->size = PACK(prev_size,FREE);
+
+       // Update Footer of current block to ne
+        HDRP(bp)->size = PACK(prev_size,FREE);
 
 
 
+        //spit();
+        //return;
+    }
+
+    // Coalesce both ends
+    else {
+    	t_block prev_HD = PREV_HDRP(bp);
+    	t_block next_FT = NEXT_FTRP(bp);
+
+    	size_t prev_size = GET_SIZE(prev_HD->size);
+    	size_t next_size = GET_SIZE(next_FT->size);
+
+        // Maintain free lists
+        //delete_from_free_list(block);
+        //delete_from_free_list(prev);
+        //delete_from_free_list(next);
+
+        // Update header of PREV & footer so NEXT
+    	prev_HD->size = PACK(prev_size + next_size,FREE);
+    	next_FT->size = PACK(prev_size + next_size,FREE);
 
 
 
-
+        //spit();
+        //return;
+    }
 
 
 	return NULL;
 }
+
+
+
+
+
+
+
+
